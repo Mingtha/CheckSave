ba3744d0e9884f7e121b4bb14c136a8a53a113da
Mingda <mingtha@qq.com>
2024-09-11 22:44:30 +0800

    存储逻辑修改

--- CheckSave.js
+++ CheckSave.js
@@ -1,42 +1,33 @@
-SUCCESS_TAG = 'CheckSave Success';
-ERROR_TAG = 'CheckSave Error';
-
-function makeRequest(request_body, response_body) {
-    var body;
-    try {
-        body = {
-            deviceId: request_body['deviceId'],
-            username: request_body['employeeNumber'],
-            x: request_body['x'],
-            y: request_body['y'],
-
-            type: response_body['type'],
-            date: response_body['data']['sysDate'],
-            location: response_body['data']['location'],
-            site: response_body['data']['site'],
-        };
-        console.log(JSON.stringify(body));
-    } catch (e) {
-        console.log(`${ERROR_TAG}, body 构造失败, ${e}`);
-    }
-}
-
-// 执行打卡
 $task.fetch($request).then(
     (response) => {
         try {
             const request_body = JSON.parse($request.body);
             const response_body = JSON.parse(response.body);
-            const myRequest = makeRequest(request_body, response_body);
-            postToServer(myRequest);
+            const checkRecord = {
+                // 请求体提取
+                deviceId: request_body['deviceId'],
+                username: request_body['employeeNumber'],
+                x: request_body['x'],
+                y: request_body['y'],
+                // 响应体提取
+                date: response_body['data']['sysDate'],
+                location: response_body['data']['location'],
+                site: response_body['data']['site'],
+            };
+            // 之前存储的记录
+            let savedRecords = $prefs.valueForKey('checkRecords') || '[]';
+            savedRecords = JSON.parse(savedRecords);
+            // 将新的记录添加到数组中
+            savedRecords.push(checkRecord);
+            // 将更新后的打卡记录持久化存储
+            $prefs.setValueForKey(JSON.stringify(savedRecords), 'checkRecords');
         } catch (e) {
-            console.log(`${ERROR_TAG}, JSON 解析失败, ${e}`);
-            $notify(ERROR_TAG, '打卡失败', 'JSON 解析失败');
+            $notify('CheckSave Error', '存储失败', e.message);
         }
         $done(response);
     },
     (reason) => {
-        $notify(ERROR_TAG, '打卡失败', reason.error);
+        $notify('CheckSave Error', '打卡失败', reason.error);
         $done();
     }
 );
